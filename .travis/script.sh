b686c1a3926ed432cc1e7babcb3a7d378f32e24b
Joachim Metz <joachim.metz@gmail.com>
2019-12-06 06:39:01 +0100

    Applied updates

--- .travis/script.sh
+++ .travis/script.sh
@@ -25,7 +25,7 @@ then
 
 		make install DESTDIR=${PWD}/osx-pkg;
 		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libagdb;
-		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libagdb;
+		cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libagdb;
 
 		pkgbuild --root osx-pkg --identifier com.github.libyal.libagdb --version ${VERSION} --ownership recommended ../libagdb-${VERSION}.pkg;
 	fi
