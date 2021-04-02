a5a8d62d75467c64230915df5539304f883222a0
Joachim Metz <joachim.metz@gmail.com>
2021-04-02 19:09:49 +0200

    Applied updates

--- tests/agdb_test_tools_output.c
+++ tests/agdb_test_tools_output.c
@@ -39,7 +39,7 @@
 
 #include "../agdbtools/agdbtools_output.h"
 
-/* Tests the agdbtools_output_initialize and function
+/* Tests the agdbtools_output_initialize function
  * Returns 1 if successful or 0 if not
  */
 int agdb_test_tools_output_initialize(
